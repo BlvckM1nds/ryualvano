ae8b13ff4532e04d1065dfb26f663b97b8ae36ba
Ryu Alvano <117072949+BlvckM1nds@users.noreply.github.com>
2023-06-15 21:09:09 +0700

    Updated location and certificates

--- script.js
+++ script.js
@@ -218,7 +218,8 @@ const certifications = [
   ['EF Standard English Test (EF SET)+EF SET English Certificate 64/100 (C1 Advanced)+2022+https://www.efset.org/cert/XAWdKJ'],
   ['SanberCode+Bootcamp ReactJS Web Frontend+2022+https://sanbercode.com/sertifikat/generate/6240c30e-4e01-4c5e-87c1-ab0c2b9f708f'],
   ['RevoU+Intro to Data Analytics+2023+https://certificates.revou.co/ryu-alvano-certificate-completion-damc23.pdf'],
-  ['365 Careers+SQL - MySQL for Data Analytics and Business Intelligence+2023+https://www.udemy.com/certificate/UC-80d02f67-03c1-4551-b167-705c749e8214/']
+  ['365 Careers+SQL - MySQL for Data Analytics and Business Intelligence+2023+https://www.udemy.com/certificate/UC-80d02f67-03c1-4551-b167-705c749e8214/'],
+  ['Udemy+The Complete JavaScript Course: From Zero to Expert!+2023+https://www.udemy.com/certificate/UC-60ba2dad-50d8-4a14-adc5-d868583bea18/']
 ];
 
 
